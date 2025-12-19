1c93186075a528c352068c82a016dbbe6d99473c
samiraa25 <reama2@ulaval.ca>
2025-12-18 23:33:34 -0500

    Enhance Demo for chi-squared and KNN analysis
    Updated the demo script for chi-squared tests and KNN with additional examples and improved plotting options.

--- demo/Demo.R
+++ demo/Demo.R
@@ -5,14 +5,14 @@ library(Equipe4.Chi2)
 
 
 
-          # Installation du package
+# Installation du package
 
 devtools::install("chemin vers le package")
 library(Equipe4.Chi2)
 
 
 
-          # Démo du chisq_table
+# Démo du chisq_table
 
 
 # 1. Importation des données
@@ -32,19 +32,39 @@ print(fonction_context)
 summary(fonction_context)
 
 
-# 5. Voici les différents plot possibles
-plot(fonction_context,"mosaique")
-plot(fonction_context,"mosaique2")
-plot(fonction_context,"heatmap")
-plot(fonction_context,"heatmap2")
-plot(fonction_context,"barplot")
-plot(fonction_context,"balloon")
+# 5. Voici les différents plot possibles. À noter que le mode LLM est uniquement possible pour le heatmp2.
+# Un avertissement sera produit si le type de graphique n'est pas heatmap2. Vous pourrez tester 
+
+plot(fonction_context, type_graphique = "mosaique", mode="llm")
+plot(fonction_context, type_graphique = "mosaique", mode="normal")
+plot(fonction_context, type_graphique = "mosaique2")
+
+plot(fonction_context, type_graphique = "heatmap")
+plot(fonction_context, type_graphique = "heatmap2")
+plot(fonction_context, type_graphique = "heatmap2", mode = "llm")
+plot(fonction_context, type_graphique = "barplot")
+plot(fonction_context, type_graphique = "balloon")
+
+plot(fonction_context, type_graphique = "heatmap", mode="llm")
+
+
+# Exemple additionnel
+data(Titanic)
+
+# Conversion en data.frame long (2201 lignes)
+df_titanic <- as.data.frame(Titanic)
+
+# Exemple : Survie × Classe
+tab_titanic <- xtabs(Freq ~ Survived + Class, data = df_titanic)
+
+obj_titanic <- chisq_table_context(tab_titanic)
+
+plot(obj_titanic, type_graphique = "mosaique", mode = "normal")
+plot(obj_titanic, type_graphique = "heatmap2", mode = "llm")  
 
 
 
-
-
-        ## Démo du KNN
+## Démo du KNN
 
 
 
